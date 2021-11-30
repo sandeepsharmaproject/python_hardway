22a7cdd0adbf337aa2e5c80f689579e9ccdd49cc
sandeep <sandeepsharmaofficial96@gmail.com>
2021-11-30 17:39:54 +0530

    excercise 2

---
+++ ex2.py
@@ -0,0 +1,4 @@
+# A comment, this is so you can read your program later
+#Anything after the # is ignored by python.
+print("I could have code like this.")# and the comment after
+print("This will run.")
