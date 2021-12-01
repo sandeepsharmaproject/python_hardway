caa2f78d1c862abfb8213e2f0facd1fabd5c88ec
sandeep <sandeepsharmaofficial96@gmail.com>
2021-12-02 01:19:30 +0530

    upto excercise7

--- ex6.py
+++ ex6.py
@@ -1,12 +1,26 @@
+# this is important pay attention.!!!
 types_of_people = 10 
 x = f"There are {types_of_people} types of people."
+
 binary = "binary"
 do_not = "don't "
 y = f"Those who know {binary} and , those who {do_not}."
 
 print(x)
 print(y)
+
 print(f"I said: {x}")
-print("")
+print("I also said: '{y}'")
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny{} "
+
+print(joke_evaluation.format(hilarious))# this is important
+
+w = "This is the left side of .."
+e = "a string with a right side."
+
+print(w + e)
+
 
      
