1fb4deeab1205f190f089d1bf5726238738b72ac
sandeep <sandeepsharmaofficial96@gmail.com>
2021-12-02 14:15:06 +0530

    excercise 21

--- ex21.py
+++ ex21.py
@@ -12,4 +12,18 @@ def divide(a,b):
     print(f"DIVIDING {a}/ {b}")
     return a / b
 
-print("Let's do some math with just functions!") 
+print("Let's do some math with just functions!")
+
+age = add(30,5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2) 
+
+print(f"Age: {age}, Height: {height} , Weight: {weight}, Iq: {divide}")
+
+#A puzzle for the extra credit , type it in anyway.
+print("Here is a puzzle.")
+
+what = add(age, subtract(height, multiply(weight, divide)))
+
+print("The becomes: ",  what, "Can you do it by hand?")
